348796d9b2ef7a6ccf9c20d90d0da51f5dd5a4b5
lucasunzaga <lucasunzaga87@gmail.com>
2025-01-15 16:12:58 -0300

    tailwind

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,24 @@
-interface homeProps {
-    titulo: string;
-    texto: string;
-}
-
-function Home(props: homeProps) {
+function Home() {
     return (
-        <div>
-            <h2>{props.titulo}</h2>
-            <p>{props.texto}</p>
-        </div>
-    );
+        <>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opniões</p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Home;
+export default Home
